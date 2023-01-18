9d2afbad1fa2059b72f31fc52e51716df48aa7b0
Carlos Pavajeau <cantte098@gmail.com>
2023-01-18 11:10:49 -0500

    refactor: move genders to constants

---
+++ constants/genders.ts
@@ -0,0 +1,12 @@
+import { RadioValue } from '@components/form/radio-group'
+
+export const genders: RadioValue[] = [
+  {
+    value: 'Male',
+    title: 'Masculino'
+  },
+  {
+    value: 'Female',
+    title: 'Femenino'
+  }
+]
