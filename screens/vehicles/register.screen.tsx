8c67c55fcbec49f705ec3443d2d6dd8786aeb5fc
Carlos Pavajeau <cantte098@gmail.com>
2023-02-11 21:38:54 -0500

    refactor: fix register vehicle ui bugs

--- screens/vehicles/register.screen.tsx
+++ screens/vehicles/register.screen.tsx
@@ -1,3 +1,4 @@
+import SafeAreaInsetsView from '@base/components/view/safe-area-insets.view'
 import { supabase } from '@base/supabase'
 import { uploadDocumentPhoto } from '@base/supabase/storage'
 import FieldError from '@components/form/feedback/field/field.error'
@@ -22,9 +23,9 @@ import {
 } from 'react-hook-form'
 import {
   Alert,
-  KeyboardAvoidingView,
   Pressable,
   ScrollView,
+  StyleSheet,
   Text,
   View
 } from 'react-native'
@@ -191,169 +192,183 @@ const RegisterVehicleScreen: FC<Props> = ({ navigation }) => {
   useFocusEffect(checkVehicle)
 
   return (
-    <FormProvider {...form}>
-      <KeyboardAvoidingView>
-        <View className="py-20 pb-2">
-          <ScrollView className="flex flex-grow w-full px-5 mx-auto space-y-3">
-            <View className="mb-5">
-              <Text className="text-xl font-medium text-center text-gray-900 dark:text-gray-200">
-                Ya solo nos faltan los datos de tu vehículo
-              </Text>
-            </View>
-
-            <View className="flex flex-row space-x-2">
-              <View className="basis-1/2">
-                <Input
-                  name="license_plate"
-                  label="Placa"
-                  disabled={isDisabled}
-                />
+    <SafeAreaInsetsView>
+      <FormProvider {...form}>
+        <ScrollView
+          className="min-h-screen px-1 py-3"
+          contentContainerStyle={styles.container}
+        >
+          <View>
+            <View className="flex flex-grow w-full px-1.5 justify-center mx-auto space-y-3">
+              <View>
+                <Text className="text-xl font-medium text-center text-gray-900 dark:text-gray-200">
+                  Ya solo nos faltan los datos de tu vehículo
+                </Text>
               </View>
-              <View className="basis-1/2">
+
+              <View className="flex flex-row space-x-2">
+                <View className="basis-1/2">
+                  <Input
+                    name="license_plate"
+                    label="Placa"
+                    disabled={isDisabled}
+                  />
+                </View>
+                <View className="basis-1/2">
+                  <Input
+                    name="engine_displacement"
+                    label="Cilindraje (CC)"
+                    keyboardType="numeric"
+                    disabled={isDisabled}
+                  />
+                </View>
+              </View>
+
+              <View className="flex flex-row space-x-2">
+                <View className="basis-1/2">
+                  <Input name="brand" label="Marca" disabled={isDisabled} />
+                </View>
+
+                <View className="basis-1/2">
+                  <Input
+                    name="line"
+                    label="Línea/Referencia"
+                    disabled={isDisabled}
+                  />
+                </View>
+              </View>
+
+              <View className="flex flex-row space-x-2">
+                <View className="basis-1/2">
+                  <Input name="model" label="Modelo" disabled={isDisabled} />
+                </View>
+
+                <View className="basis-1/2">
+                  <Input name="color" label="Color" disabled={isDisabled} />
+                </View>
+              </View>
+
+              <Controller
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <View className="mt-2">
+                    <Text className="font-medium text-gray-700 dark:text-gray-300 mb-2">
+                      Tipo
+                    </Text>
+                    <RadioGroup
+                      values={vehicleTypes}
+                      selected={value}
+                      onSelect={onChange}
+                    />
+
+                    {errors.type != null && (
+                      <FieldError message={errors.type.message} />
+                    )}
+                  </View>
+                )}
+                name="type"
+              />
+
+              <View>
                 <Input
-                  name="engine_displacement"
-                  label="Cilindraje (CC)"
+                  name="owner_id"
+                  label="N° documento del propietario"
                   keyboardType="numeric"
                   disabled={isDisabled}
                 />
               </View>
-            </View>
 
-            <View className="flex flex-row space-x-2">
-              <View className="basis-1/2">
-                <Input name="brand" label="Marca" disabled={isDisabled} />
-              </View>
-
-              <View className="basis-1/2">
-                <Input
-                  name="line"
-                  label="Línea/Referencia"
-                  disabled={isDisabled}
-                />
-              </View>
-            </View>
-
-            <View className="flex flex-row space-x-2">
-              <View className="basis-1/2">
-                <Input name="model" label="Modelo" disabled={isDisabled} />
-              </View>
-
-              <View className="basis-1/2">
-                <Input name="color" label="Color" disabled={isDisabled} />
-              </View>
-            </View>
-
-            <Controller
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <View className="mt-2">
-                  <Text className="font-medium text-gray-700 dark:text-gray-300 mb-2">
-                    Tipo
-                  </Text>
-                  <RadioGroup
-                    values={vehicleTypes}
-                    selected={value}
-                    onSelect={onChange}
-                  />
-
-                  {errors.type != null && (
-                    <FieldError message={errors.type.message} />
-                  )}
-                </View>
-              )}
-              name="type"
-            />
-
-            <View>
-              <Input
-                name="owner_id"
-                label="N° documento del propietario"
-                keyboardType="numeric"
-                disabled={isDisabled}
-              />
-            </View>
-
-            <View>
-              <Text className="font-medium text-gray-700 dark:text-gray-300 mb-2">
-                Tarjeta de propiedad
-              </Text>
-              <View className="flex flex-row space-x-2">
-                <View className="basis-1/2">
-                  <PhotoPicker
-                    label="Parte frontal"
-                    mode="take"
-                    disabled={isDisabled}
-                    onSelect={(photo) => {
-                      handleDocumentPhoto(
-                        'property_card_photo_url_front',
-                        photo
-                      )
-                    }}
-                  />
-
-                  {watch('property_card_photo_url_front') !== undefined && (
-                    <Text className="text-green-600 text-xs font-medium mt-0.5 dark:text-green-500">
-                      Foto cargada
-                    </Text>
-                  )}
-
-                  {errors.property_card_photo_url_front !== undefined && (
-                    <FieldError
-                      message={errors.property_card_photo_url_front.message}
-                    />
-                  )}
-                </View>
-                <View className="basis-1/2">
-                  <PhotoPicker
-                    label="Parte trasera"
-                    mode="take"
-                    disabled={isDisabled}
-                    onSelect={(photo) => {
-                      handleDocumentPhoto(
-                        'property_card_photo_url_back',
-                        photo
-                      )
-                    }}
-                  />
-
-                  {watch('property_card_photo_url_back') !== undefined && (
-                    <Text className="text-green-600 text-xs font-medium mt-0.5 dark:text-green-500">
-                      Foto cargada
-                    </Text>
-                  )}
-
-                  {errors.property_card_photo_url_back !== undefined && (
-                    <FieldError
-                      message={errors.property_card_photo_url_back.message}
-                    />
-                  )}
-                </View>
-              </View>
-            </View>
-
-            {error !== null && <FieldError message={errorText} />}
-
-            <View>
-              <Pressable
-                onPress={handleSubmit(onSubmit)}
-                disabled={isDisabled}
-                className={cn(
-                  'text-base px-6 py-3.5 bg-blue-700 rounded-lg border border-transparent',
-                  'active:bg-blue-800',
-                  isDisabled && 'bg-gray-300 text-gray-700 cursor-not-allowed',
-                  isDisabled && 'dark:bg-gray-800 dark:text-gray-400'
-                )}
-              >
-                <Text className="text-base font-medium text-center text-white">
-                  Enviar
+              <View>
+                <Text className="font-medium text-gray-700 dark:text-gray-300 mb-2">
+                  Tarjeta de propiedad
                 </Text>
-              </Pressable>
+                <View className="flex flex-row justify-center space-x-2">
+                  <View className="basis-1/2">
+                    <PhotoPicker
+                      label="Parte frontal"
+                      mode="take"
+                      disabled={isDisabled}
+                      onSelect={(photo) => {
+                        handleDocumentPhoto(
+                          'property_card_photo_url_front',
+                          photo
+                        )
+                      }}
+                    />
+
+                    {watch('property_card_photo_url_front') !== undefined && (
+                      <Text className="text-green-600 text-xs font-medium mt-0.5 dark:text-green-500">
+                        Foto cargada
+                      </Text>
+                    )}
+
+                    {errors.property_card_photo_url_front !== undefined && (
+                      <FieldError
+                        message={errors.property_card_photo_url_front.message}
+                      />
+                    )}
+                  </View>
+                  <View className="basis-1/2">
+                    <PhotoPicker
+                      label="Parte trasera"
+                      mode="take"
+                      disabled={isDisabled}
+                      onSelect={(photo) => {
+                        handleDocumentPhoto(
+                          'property_card_photo_url_back',
+                          photo
+                        )
+                      }}
+                    />
+
+                    {watch('property_card_photo_url_back') !== undefined && (
+                      <Text className="text-green-600 text-xs font-medium mt-0.5 dark:text-green-500">
+                        Foto cargada
+                      </Text>
+                    )}
+
+                    {errors.property_card_photo_url_back !== undefined && (
+                      <FieldError
+                        message={errors.property_card_photo_url_back.message}
+                      />
+                    )}
+                  </View>
+                </View>
+              </View>
+
+              {error !== null && <FieldError message={errorText} />}
+
+              <View className="pt-3">
+                <Pressable
+                  onPress={handleSubmit(onSubmit)}
+                  disabled={isDisabled}
+                  className={cn(
+                    'text-base px-6 py-3.5 bg-blue-700 rounded-lg border border-transparent',
+                    'active:bg-blue-800',
+                    isDisabled &&
+                      'bg-gray-300 text-gray-700 cursor-not-allowed',
+                    isDisabled && 'dark:bg-gray-800 dark:text-gray-400'
+                  )}
+                >
+                  <Text className="text-base font-medium text-center text-white">
+                    Enviar
+                  </Text>
+                </Pressable>
+              </View>
             </View>
-          </ScrollView>
-        </View>
-      </KeyboardAvoidingView>
-    </FormProvider>
+          </View>
+        </ScrollView>
+      </FormProvider>
+    </SafeAreaInsetsView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default RegisterVehicleScreen
