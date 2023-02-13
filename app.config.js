dad004f49935824c923d7b16c48b00bf7917e3c4
Carlos Pavajeau <cantte098@gmail.com>
2023-02-13 10:50:58 -0500

    chore: update app version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'MrChoffer',
     slug: 'mr-chofer',
-    version: '1.0.2',
+    version: '1.1.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'myapp',
