72300c5a5e7ec8509beee8876b174378711ad40a
Carlos Pavajeau <cantte098@gmail.com>
2023-02-14 11:50:23 -0500

    feat: make vibration when new request is made

--- rides/hooks/realtime/use-realtime-requested-rides.ts
+++ rides/hooks/realtime/use-realtime-requested-rides.ts
@@ -1,15 +1,16 @@
-import { useEffect } from 'react'
+import useCurrentDriverRide from '@base/rides/hooks/use-current-driver-ride'
 import {
   NewRidesChannel,
   RideChangesChannel
 } from '@base/rides/realtime/channels'
+import { RideStatus } from '@base/rides/types'
 import {
   REALTIME_LISTEN_TYPES,
   REALTIME_POSTGRES_CHANGES_LISTEN_EVENT
 } from '@supabase/supabase-js'
-import { RideStatus } from '@base/rides/types'
 import { useQueryClient } from '@tanstack/react-query'
-import useCurrentDriverRide from '@base/rides/hooks/use-current-driver-ride'
+import { useEffect } from 'react'
+import { Vibration } from 'react-native'
 
 const useRealtimeRequestedRides = () => {
   const { ride, isLoading } = useCurrentDriverRide()
@@ -27,53 +28,59 @@ const useRealtimeRequestedRides = () => {
     const newRidesChannel = NewRidesChannel()
     const rideChangesChannel = RideChangesChannel()
 
-    newRidesChannel.on(
-      REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
-      {
-        event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.INSERT,
-        schema: 'public',
-        table: 'rides',
-        filter: `status=eq.${RideStatus.requested}`
-      },
-      () => {
-        void queryClient.invalidateQueries(['requested-rides'])
-      }
-    ).subscribe()
+    newRidesChannel
+      .on(
+        REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
+        {
+          event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.INSERT,
+          schema: 'public',
+          table: 'rides',
+          filter: `status=eq.${RideStatus.requested}`
+        },
+        () => {
+          Vibration.vibrate(1000)
+          void queryClient.invalidateQueries(['requested-rides'])
+        }
+      )
+      .subscribe()
 
-    rideChangesChannel.on(
-      REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
-      {
-        event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE,
-        schema: 'public',
-        table: 'rides',
-        filter: `status=eq.${RideStatus.accepted}`
-      },
-      () => {
-        void queryClient.invalidateQueries(['requested-rides'])
-      }
-    ).on(
-      REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
-      {
-        event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE,
-        schema: 'public',
-        table: 'rides',
-        filter: `status=eq.${RideStatus.canceled}`
-      },
-      () => {
-        void queryClient.invalidateQueries(['requested-rides'])
-      }
-    ).on(
-      REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
-      {
-        event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE,
-        schema: 'public',
-        table: 'rides',
-        filter: `status=eq.${RideStatus.requested}`
-      },
-      () => {
-        void queryClient.invalidateQueries(['requested-rides'])
-      }
-    )
+    rideChangesChannel
+      .on(
+        REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
+        {
+          event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE,
+          schema: 'public',
+          table: 'rides',
+          filter: `status=eq.${RideStatus.accepted}`
+        },
+        () => {
+          void queryClient.invalidateQueries(['requested-rides'])
+        }
+      )
+      .on(
+        REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
+        {
+          event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE,
+          schema: 'public',
+          table: 'rides',
+          filter: `status=eq.${RideStatus.canceled}`
+        },
+        () => {
+          void queryClient.invalidateQueries(['requested-rides'])
+        }
+      )
+      .on(
+        REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
+        {
+          event: REALTIME_POSTGRES_CHANGES_LISTEN_EVENT.UPDATE,
+          schema: 'public',
+          table: 'rides',
+          filter: `status=eq.${RideStatus.requested}`
+        },
+        () => {
+          void queryClient.invalidateQueries(['requested-rides'])
+        }
+      )
       .subscribe()
 
     return () => {
